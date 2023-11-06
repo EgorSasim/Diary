f10d901a63c89201ee9552dc50266132461f839c
Egor Sasim <egorsasim@mail.ru>
2023-11-06 16:42:43 +0300

    add block for creation spaces and finish first fersion of side pannel, DFT-2

--- src/app/modules/home/side-pannel/side-pannel.component.ts
+++ src/app/modules/home/side-pannel/side-pannel.component.ts
@@ -6,4 +6,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./side-pannel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidePannelComponent {}
+export class SidePannelComponent {
+  private readonly closePannelSign: string = '<<';
+  private readonly openPannelSign: string = '>>';
+  public readonly closeSpacesSign: string = 'V';
+  public readonly openSpaceSign: string = '>';
+
+  public pannelSign = this.closePannelSign;
+  public pannelIsOpened = true;
+  public spaceSign = this.openSpaceSign;
+  public spacesAreOpened = false;
+
+  public togglePannel(): void {
+    this.pannelSign =
+      this.pannelSign === this.closePannelSign
+        ? this.openPannelSign
+        : this.closePannelSign;
+    this.pannelIsOpened = !this.pannelIsOpened;
+  }
+
+  public toggleSpaces(): void {
+    this.spaceSign =
+      this.spaceSign === this.closeSpacesSign
+        ? this.openSpaceSign
+        : this.closeSpacesSign;
+    this.spacesAreOpened = !this.spacesAreOpened;
+  }
+}
