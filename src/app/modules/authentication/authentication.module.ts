82194673eededc3a2d112634c66853596ab0aebf
Egor Sasim <egorsasim@mail.ru>
2023-12-07 00:54:06 +0300

    remove unused styles, DFT-4

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthenticationComponent } from 'src/app/modules/authentication/authentication.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AuthenticationComponent],
-  imports: [ReactiveFormsModule, TranslateModule],
+  imports: [ReactiveFormsModule, TranslateModule, MatButtonModule],
   exports: [AuthenticationComponent],
 })
 export class AuthenticationModule {}
